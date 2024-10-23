a8434e951d24aed211dcec020ca97f1dbcbf6c01
Matias Vazquez <matias.vazquezdelacolina@gmail.com>
2024-10-23 02:07:22 -0300

    clean unused boiler code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang='en'>
-      <Head />
+    <Html lang='es'>
+      <Head>
+        <title>Dux Challenge</title>
+        <meta
+          name='description'
+          content='technical front-end challenge dux software'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
