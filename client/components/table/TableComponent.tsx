1f521badbb292ae1f7db4201999423127613cf08
matias-vdlc <matias.vazquezdelacolina@gmail.com>
2024-10-28 09:23:10 -0300

    table component

--- client/components/table/TableComponent.tsx
+++ client/components/table/TableComponent.tsx
@@ -1,61 +1,83 @@
-import { FC, useEffect, useState } from 'react'
+import { FC, useState } from 'react'
 import { DataTable } from 'primereact/datatable'
 import { Column } from 'primereact/column'
-import { Paginator } from 'primereact/paginator'
+import { Paginator, PaginatorPageChangeEvent } from 'primereact/paginator'
 import { TableHeader } from './tableHeader'
 import { Button } from 'primereact/button'
 
-const headerButtons = [
-  {
-    label: 'Nuevo Usuario',
-    icon: 'pi pi-plus',
-    className: 'gap-2 p-2 border-round-md',
-    onClick: () => {},
-  },
-]
+// TODO: type correctly
+type TableComponentProps = {
+  data: Record<string, any>[]
+  title: string
+  tableHeaders: {
+    field: string
+    header: string
+    sortable?: boolean
+    className: string
+    style?: Record<string, any>
+  }[]
+  headerButtons: {
+    label: string
+    icon: string
+    className: string
+    onClick: () => void
+  }[]
+  rowActions?: {
+    'aria-label': string
+    icon: string
+    className: string
+    onClick: (rowData: any) => void
+  }[]
+  paginator: {
+    first: number
+    rows: number
+    page: number
+    pageCount: number
+    totalRecords: number
+  }
+  setPaginator: (event: any) => void
+}
 
-const rowActions = [
-  {
-    ['aria-label']: 'Editar',
-    icon: 'pi pi-pencil',
-    className: 'gap-2 p-2 border-round-md',
-    onClick: () => {},
-  },
-  {
-    ['aria-label']: 'Eliminar',
-    icon: 'pi pi-trash',
-    className: 'gap-2 p-2 border-round-md',
-    onClick: () => {},
-  },
-]
-
-export const TableComponent: FC<{ data: any; header: string }> = ({
+export const TableComponent: FC<TableComponentProps> = ({
   data,
-  header,
+  title,
+  headerButtons = [],
+  tableHeaders = [],
+  rowActions = [],
+  paginator,
+  setPaginator,
 }) => {
-  const [content, setContent] = useState(data || [])
   const [globalFilter, setGlobalFilter] = useState<string | null>(null)
 
-  useEffect(() => {
-    if (!data) return
-    setContent(data)
-  }, [data])
-
-  // TODO: remove log
-  console.log({ data })
-  // return <div>TableComponent</div>
+  const onPageChange = (event: PaginatorPageChangeEvent) => {
+    setPaginator(
+      (prev: {
+        first: number
+        rows: number
+        page: number
+        pageCount: number
+        totalRecords: number
+      }) => ({
+        ...prev,
+        first: event.first,
+        rows: event.rows,
+        page: event.page,
+        // pageCount: event.pageCount,
+      }),
+    )
+  }
 
   const actionBodyTemplate = (rowData: any) => {
     return (
       <div className='flex gap-2'>
-        {rowActions.map((action) => (
+        {rowActions?.map((action, index) => (
           <Button
-            key={`id-${rowData.id}`}
+            key={`id-${index}`}
             icon={action.icon}
             aria-label={action['aria-label']}
             rounded
             text
-            onClick={action.onClick}
+            onClick={() => action.onClick(rowData)}
           />
         ))}
       </div>
@@ -64,221 +86,55 @@ export const TableComponent: FC<{ data: any; header: string }> = ({
 
   return (
     <div
-      className='px-4 py-3 flex flex-column  justify-content-between'
+      className='px-4 py-3 flex flex-column  justify-content-between gap-4'
       style={{ height: '100%' }}
     >
       <div className='flex flex-column gap-2'>
         <TableHeader
-          header={header}
+          title={title}
           headerActions={headerButtons}
           onSearch={setGlobalFilter}
         />
 
         <DataTable
-          value={content}
+          value={data}
           dataKey='id'
-          rows={5}
           globalFilterFields={['usuario', 'estado']}
           globalFilter={globalFilter}
           className='p-datatable-sm'
           removableSort
+          scrollable
+          scrollHeight='600px'
         >
-          <Column
-            field='id'
-            header='ID'
-            className='text-sm line-height-1 text-color-secondary capitalize'
-          />
-          <Column
-            field='usuario'
-            header='Nombre'
-            sortable
-            className='text-sm text-primary font-bold underline line-height-1'
-          />
-
-          <Column
-            field='estado'
-            header='Estado'
-            sortable
-            className='text-sm line-height-1 text-color-secondary capitalize'
-          />
-          <Column
-            field='sector'
-            header='Sector'
-            className='text-sm line-height-1 text-color-secondary capitalize'
-          />
-          <Column
-            body={actionBodyTemplate}
-            exportable={false}
-            className='flex justify-content-center'
-          />
+          {!!tableHeaders.length &&
+            tableHeaders.map((th, index) => (
+              <Column
+                key={`id-${index}`}
+                field={th.field}
+                header={th.header}
+                sortable={th.sortable}
+                className={th.className}
+              />
+            ))}
+          {!!rowActions.length && (
+            <Column
+              field='actions'
+              header='Acciones'
+              body={actionBodyTemplate}
+              exportable={false}
+            />
+          )}
         </DataTable>
       </div>
-
-      {/* <Dialog
-        visible={productDialog}
-        breakpoints={{ '960px': '75vw', '640px': '100vw' }}
-        style={{ width: '40vw' }}
-        header='Product Details'
-        modal
-        className='p-fluid'
-        footer={productDialogFooter}
-        onHide={hideDialog}
-      >
-        {product.image && (
-          <img
-            src={`demo/images/product/${product.image}`}
-            onError={(e) =>
-              (e.target.src =
-                'https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png')
-            }
-            alt={product.image}
-            className='block mt-0 mx-auto mb-5 w-20rem shadow-2'
-          />
-        )}
-        <div className='field'>
-          <label htmlFor='name'>Name</label>
-          <InputText
-            id='name'
-            value={product.name}
-            onChange={(e) => onInputChange(e, 'name')}
-            required
-            autoFocus
-            className={classNames({ 'p-invalid': submitted && !product.name })}
-          />
-          {submitted && !product.name && (
-            <small className='p-error'>Name is required.</small>
-          )}
-        </div>
-        <div className='field'>
-          <label htmlFor='description'>Description</label>
-          <InputTextarea
-            id='description'
-            value={product.description}
-            onChange={(e) => onInputChange(e, 'description')}
-            required
-            rows={3}
-            cols={20}
-          />
-        </div>
-
-        <div className='field'>
-          <label className='mb-3'>Category</label>
-          <div className='formgrid grid'>
-            <div className='field-radiobutton col-6'>
-              <RadioButton
-                inputId='category1'
-                name='category'
-                value='Accessories'
-                onChange={onCategoryChange}
-                checked={product.category === 'Accessories'}
-              />
-              <label htmlFor='category1'>Accessories</label>
-            </div>
-            <div className='field-radiobutton col-6'>
-              <RadioButton
-                inputId='category2'
-                name='category'
-                value='Clothing'
-                onChange={onCategoryChange}
-                checked={product.category === 'Clothing'}
-              />
-              <label htmlFor='category2'>Clothing</label>
-            </div>
-            <div className='field-radiobutton col-6'>
-              <RadioButton
-                inputId='category3'
-                name='category'
-                value='Electronics'
-                onChange={onCategoryChange}
-                checked={product.category === 'Electronics'}
-              />
-              <label htmlFor='category3'>Electronics</label>
-            </div>
-            <div className='field-radiobutton col-6'>
-              <RadioButton
-                inputId='category4'
-                name='category'
-                value='Fitness'
-                onChange={onCategoryChange}
-                checked={product.category === 'Fitness'}
-              />
-              <label htmlFor='category4'>Fitness</label>
-            </div>
-          </div>
-        </div>
-
-        <div className='formgrid grid'>
-          <div className='field col'>
-            <label htmlFor='price'>Price</label>
-            <InputNumber
-              id='price'
-              value={product.price}
-              onValueChange={(e) => onInputNumberChange(e, 'price')}
-              mode='currency'
-              currency='USD'
-              locale='en-US'
-            />
-          </div>
-          <div className='field col'>
-            <label htmlFor='quantity'>Quantity</label>
-            <InputNumber
-              id='quantity'
-              value={product.quantity}
-              onValueChange={(e) => onInputNumberChange(e, 'quantity')}
-              integeronly
-            />
-          </div>
-        </div>
-      </Dialog> */}
-
-      {/* <Dialog
-        visible={deleteProductDialog}
-        style={{ width: '450px' }}
-        header='Confirm'
-        modal
-        footer={deleteProductDialogFooter}
-        onHide={hideDeleteProductDialog}
-      >
-        <div className='flex align-items-center justify-content-center'>
-          <i
-            className='pi pi-exclamation-triangle mr-3'
-            style={{ fontSize: '2rem' }}
-          />
-          {product && (
-            <span>
-              Are you sure you want to delete <b>{product.name}</b>?
-            </span>
-          )}
-        </div>
-      </Dialog> */}
-
-      {/* <Dialog
-        visible={deleteProductsDialog}
-        style={{ width: '450px' }}
-        header='Confirm'
-        modal
-        footer={deleteProductsDialogFooter}
-        onHide={hideDeleteProductsDialog}
-      >
-        <div className='flex align-items-center justify-content-center'>
-          <i
-            className='pi pi-exclamation-triangle mr-3'
-            style={{ fontSize: '2rem' }}
-          />
-          {product && (
-            <span>Are you sure you want to delete the selected products?</span>
-          )}
-        </div>
-      </Dialog> */}
 
       {/* // TODO: make paginator component */}
       <div>
         <Paginator
-          first={1}
-          rows={10}
-          totalRecords={120}
-          rowsPerPageOptions={[5, 10, 25, 50]}
-          onPageChange={() => {}}
+          first={paginator?.first}
+          rows={paginator?.rows}
+          totalRecords={paginator?.totalRecords}
+          rowsPerPageOptions={[5, 10, 20]}
+          onPageChange={onPageChange}
         />
       </div>
     </div>
--- client/views/users/UsersTable.tsx
+++ client/views/users/UsersTable.tsx
@@ -1,6 +1,316 @@
+import { useEffect, useState, useRef } from 'react'
 import { TableComponent } from '../../components/table'
 import { User } from '../../interface'
+import {
+  useCreateUser,
+  useGetUsers,
+  useUpdateUser,
+  useDeleteUser,
+} from '../../api'
+import { tableHeaders } from './constants'
+import { Dialog } from 'primereact/dialog'
+import { Button } from 'primereact/button'
+import { colors } from '../../styles/colors'
+import { InputText } from 'primereact/inputtext'
+import { Dropdown } from 'primereact/dropdown'
+import { status } from './constants'
 
 export const UsersTable = ({ users }: { users: User[] }) => {
-  return <TableComponent data={users} header='Usuarios' />
+  const [data, setData] = useState<User[]>(users)
+  const [selectedUser, setSelectedUser] = useState<User | null>(null)
+  const [paginator, setPaginator] = useState({
+    first: 1,
+    rows: 5,
+    page: 1,
+    pageCount: 0,
+    totalRecords: 100,
+  })
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false)
+  const [showCreateDialog, setShowCreateDialog] = useState(false)
+  const [isEdit, setIsEdit] = useState(false)
+  // const [submitted, setSubmitted] = useState(false)
+  const [isFormValid, setIsFormValid] = useState(true)
+  const initialUserRef = useRef<User | null>(null)
+
+  const { handler: userCreateHandler, isLoading: userCreateLoading } =
+    useCreateUser()
+  const {
+    handler: userGetHandler,
+    data: updatedUserData,
+    isLoading: userGetLoading,
+  } = useGetUsers()
+  const { handler: userUpdateHandler, isLoading: userUpdateLoading } =
+    useUpdateUser()
+  const { handler: userDeleteHandler, isLoading: userDeleteLoading } =
+    useDeleteUser()
+
+  const handleDeleteUserAction = (user: User) => {
+    setShowDeleteDialog(true)
+    setSelectedUser(user)
+  }
+
+  const hideDeleteUserDialog = () => {
+    setShowDeleteDialog(false)
+    setSelectedUser(null)
+  }
+
+  const handleDeleteUser = () => {
+    if (!selectedUser) return
+    userDeleteHandler(selectedUser.id)
+    hideDeleteUserDialog()
+  }
+
+  const handleCreateUserAction = () => {
+    setSelectedUser(null)
+    setShowCreateDialog(true)
+  }
+
+  const handleEditUserAction = (user: User) => {
+    setSelectedUser(user)
+    initialUserRef.current = user
+    setShowCreateDialog(true)
+    setIsEdit(true)
+  }
+
+  const isFormIncomplete = () => {
+    return (
+      !selectedUser?.id ||
+      !selectedUser?.sector ||
+      !selectedUser?.usuario ||
+      !selectedUser?.estado
+    )
+  }
+
+  const handleSubmitModal = () => {
+    if (!selectedUser) return
+
+    if (isEdit) {
+      const updatedFields: Partial<User> = {}
+
+      // Compare each field to find changes
+      Object.keys(selectedUser).forEach((key) => {
+        const k = key as keyof User
+        if (selectedUser[k] !== initialUserRef.current?.[k]) {
+          updatedFields[k] = selectedUser[k]
+        }
+      })
+
+      if (Object.keys(updatedFields).length > 0) {
+        userUpdateHandler(initialUserRef.current?.id!, updatedFields)
+      }
+      setIsFormValid(true)
+      setIsEdit(false)
+    } else {
+      if (isFormIncomplete()) {
+        setIsFormValid(false)
+        return
+      }
+
+      userCreateHandler(selectedUser)
+    }
+    setShowCreateDialog(false)
+  }
+
+  const onInputChange = (value, name) => {
+    setSelectedUser({ ...selectedUser, [name]: value })
+  }
+
+  // TODO: add user
+  const headerButtons = [
+    {
+      label: 'Nuevo Usuario',
+      icon: 'pi pi-plus',
+      className: 'gap-2 p-2 border-round-md',
+      onClick: handleCreateUserAction,
+    },
+  ]
+
+  // TODO: add actions edit and delete
+  const rowActions = [
+    {
+      ['aria-label']: 'Editar',
+      icon: 'pi pi-pencil text-sm',
+      className: 'gap-2 border-round-md',
+      onClick: (rowData: User) => handleEditUserAction(rowData),
+    },
+    {
+      ['aria-label']: 'Eliminar',
+      icon: 'pi pi-trash text-sm',
+      className: 'gap-2 border-round-md',
+      onClick: (rowData: User) => handleDeleteUserAction(rowData),
+    },
+  ]
+
+  const deleteUserDialogFooter = (
+    <div className='flex align-items-center justify-content-end gap-2'>
+      <Button
+        label='Si'
+        icon='pi pi-check'
+        severity='danger'
+        onClick={handleDeleteUser}
+      />
+      <Button
+        label='No'
+        icon='pi pi-times'
+        outlined
+        className='text-color'
+        onClick={hideDeleteUserDialog}
+      />
+    </div>
+  )
+
+  const headerCreateDialog = (
+    <div className='flex align-items-center justify-content-start bg-primary h-3rem m-0'>
+      <div className='font-bold'>{isEdit ? 'Editar usuario' : 'Usuario'}</div>
+    </div>
+  )
+
+  const footerCreateDialog = (
+    <div className='flex align-items-center justify-content-center gap-2'>
+      <Button
+        label='Confirmar'
+        icon='pi pi-check'
+        className='border-round-md'
+        style={{
+          backgroundColor: colors.BUTTON,
+        }}
+        autoFocus
+        raised
+        onClick={handleSubmitModal}
+      />
+      <Button
+        label='Cancelar'
+        outlined
+        icon='pi pi-check'
+        className='border-round-md'
+        style={{
+          color: colors.BUTTON,
+        }}
+        autoFocus
+        onClick={() => setShowCreateDialog(false)}
+      />
+    </div>
+  )
+
+  // TODO: validate paginator
+  useEffect(() => {
+    if (!paginator.page || !paginator.rows) return
+    if (paginator.first === 1) return
+    const params = {
+      sector: 2000,
+      page: paginator.page,
+      limit: paginator.rows,
+    }
+    userGetHandler(params)
+  }, [paginator.page, paginator.rows])
+
+  useEffect(() => {
+    if (userDeleteLoading) return
+    if (updatedUserData) {
+      setData(updatedUserData)
+    }
+  }, [updatedUserData])
+
+  useEffect(() => {
+    if (userCreateLoading || userUpdateLoading || userDeleteLoading) return
+    userGetHandler({ sector: 2000, page: 1, limit: 5 })
+  }, [userDeleteLoading, userCreateLoading, userUpdateLoading])
+
+  return (
+    <>
+      <TableComponent
+        data={data}
+        title='Usuarios'
+        headerButtons={headerButtons}
+        tableHeaders={tableHeaders}
+        rowActions={rowActions}
+        paginator={paginator}
+        setPaginator={setPaginator}
+        
+      />
+      <Dialog
+        visible={showCreateDialog}
+        modal
+        header={headerCreateDialog}
+        footer={footerCreateDialog}
+        style={{ width: '1100px' }}
+        onHide={() => setShowCreateDialog(false)}
+        className='p-fluid'
+      >
+        <div className='flex flex-column gap-4'>
+          <div className='field'>
+            <label htmlFor='id' className='mb-2 font-semibold'>
+              id
+            </label>
+            <InputText
+              id='id'
+              value={selectedUser?.id?.toString() || ''}
+              onChange={(e) => onInputChange(e.target.value, 'id')}
+              required
+              autoFocus
+            />
+          </div>
+          <div className='field'>
+            <label htmlFor='usuario' className='mb-2 font-semibold'>
+              Nombre
+            </label>
+            <InputText
+              id='usuario'
+              value={selectedUser?.usuario || ''}
+              onChange={(e) => onInputChange(e.target.value, 'usuario')}
+              required
+              autoFocus
+            />
+          </div>
+          <div className='field'>
+            <label htmlFor='estado' className='mb-2 font-semibold'>
+              Estado
+            </label>
+            <Dropdown
+              value={selectedUser?.estado || ''}
+              onChange={(e) => onInputChange(e.target.value, 'estado')}
+              options={status}
+              optionLabel='status'
+              placeholder='Selecciona el Estado'
+              className='w-full border-round-md'
+            />
+          </div>
+          <div className='field'>
+            <label htmlFor='sector' className='mb-2 font-semibold'>
+              Sector
+            </label>
+            <InputText
+              id='sector'
+              value={selectedUser?.sector?.toString() || ''}
+              onChange={(e) => onInputChange(e.target.value, 'sector')}
+              required
+              autoFocus
+            />
+          </div>
+        </div>
+      </Dialog>
+
+      <Dialog
+        visible={showDeleteDialog}
+        style={{ width: '450px' }}
+        header='Eliminar usuario'
+        modal
+        footer={deleteUserDialogFooter}
+        onHide={hideDeleteUserDialog}
+      >
+        <div className='flex align-items-center justify-content-center gap-2'>
+          <i
+            className='pi pi-exclamation-triangle mr-3 text-red-500'
+            style={{ fontSize: '2rem' }}
+          />
+          {selectedUser && (
+            <span>
+              ¿Está seguro que desea eliminar el usuario{' '}
+              <b>{selectedUser.usuario}</b>?
+            </span>
+          )}
+        </div>
+      </Dialog>
+    </>
+  )
 }
